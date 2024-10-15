0f4d3f713e491bd2549ba255eca38af55fe42e8b
Adrian Nunez <44225495+r3k4ce@users.noreply.github.com>
2024-10-15 16:48:09 -0300

    Complete Exercises 01 02 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // Return ERROR if number is negative
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    // Repeat string the specified amount of num
+    let finalString = ""
+    for (i = 0; i < num; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let stringReverse = '';
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        stringReverse += stringArray[i];
+    }
+    return stringReverse;
 };
 
 // Do not edit below this line
