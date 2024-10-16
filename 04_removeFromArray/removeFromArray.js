03c69d46547289b1e655f961aae5df5167ec18e3
Adrian Nunez <44225495+r3k4ce@users.noreply.github.com>
2024-10-16 14:20:37 -0300

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...nums) {
+    let filteredList = [];
+    // loop thorough nums list
+    for (let num of nums) {
+        // for each num check wether it is in list
+        if (list.includes(num)) {
+            // if num is in list remove all ocurrences
+            list = list.filter((item) => item!== num);
+            filteredList = list;
+        } else {
+            filteredList = list;
+        }
+    }
+
+    return filteredList;
 };
 
 // Do not edit below this line
