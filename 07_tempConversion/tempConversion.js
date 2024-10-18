cd7bf536fc4a45d99396056dd8bf0fee6df30a4e
Adrian <44225495+r3k4ce@users.noreply.github.com>
2024-10-18 13:55:38 -0300

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  // C = (F - 32) * 5/9
+  result = (f - 32) * 5/9;
+  if (result === 0) {
+    return 0
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  // F = (C * 9/5) + 32
+  result = (c * 9/5) + 32;
+  if (result === 0) {
+    return 0
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
